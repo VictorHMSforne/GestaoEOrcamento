233b3d48d77880393eb9b9cd7538bdfbf596bf88
Victor <vmendessforne@gmail.com>
2025-02-25 18:40:44 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoEOrcamento/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GestaoEOrcamento
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
